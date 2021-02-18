f61af4d6dd84038f1db6d3cee4f52871a9d37c55
MINSOO KANG <stengine2@gmail.com>
2021-02-18 13:36:40 +0900

    optimizer outline

---
+++ optimizer.py
@@ -0,0 +1,12 @@
+import circuit
+
+# counts_distribution =  circuit.evaluate(parameter, shot_size)
+
+def optimize_circuit():
+	# take step
+	
+	# evaluate and compare
+	
+	# change current state to the optimal step and retry
+	# if it's local optima, try smaller step
+	# check termination criteria
