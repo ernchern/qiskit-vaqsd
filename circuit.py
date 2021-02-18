05e2428c2d40fc58130369718d10a6f710418c16
Assem Zhunis <zhunis.assem@gmail.com>
2021-02-18 12:12:47 +0900

    add build_circuit function

--- circuit.py
+++ circuit.py
@@ -5,39 +5,43 @@ from qiskit import(
   QuantumCircuit,
   execute,
   Aer)
-from qiskit.visualization import plot_histogram
 
-import numpy as np
-np.random.seed(99999)
 
-# Initialize the matrix
-params = np.random.rand(3)
+def build_circuit(params,shots=1000):
+    '''
+    Inputs:
+    
+        params: numpy array of [theta, phi, lam]
+        shots: number of executions of the circuit
+    Output:
+        out_counts: numpy array of probabilities of outcomes in the order: '00', '01', '10', '11'
+    '''
+    # Use Aer's qasm_simulator
+    simulator = Aer.get_backend('qasm_simulator')
 
-# Use Aer's qasm_simulator
-simulator = Aer.get_backend('qasm_simulator')
+    # Create a Quantum Circuit acting on the q register
+    circuit = QuantumCircuit(2, 2)
 
-# Create a Quantum Circuit acting on the q register
-circuit = QuantumCircuit(2, 2)
+    # Add a H gate on qubit 1
+    circuit.h(1)
 
-# Add a H gate on qubit 1
-circuit.h(1)
+    # # Add a U3 gate with random parameters
+    circuit.u3(params[0],params[1],params[2],0)
+    circuit.u3(params[0],params[1],params[2],1)
 
-# # Add a U3 gate with random parameters
-circuit.u3(params[0],params[1],params[2],0)
-circuit.u3(params[0],params[1],params[2],1)
+    # Map the quantum measurement to the classical bits
+    circuit.measure([0,1], [0,1])
 
-# Map the quantum measurement to the classical bits
-circuit.measure([0,1], [0,1])
+    # Execute the circuit on the qasm simulator
+    job = execute(circuit, simulator, shots=shots)
 
-# Execute the circuit on the qasm simulator
-job = execute(circuit, simulator, shots=1000)
+    # Grab results from the job
+    result = job.result()
 
-# Grab results from the job
-result = job.result()
+    # Returns counts
+    counts = result.get_counts(circuit)
+    
+    out_counts = np.array([counts['00'],counts['01'],counts['10'],counts['11']])/shots
 
-# Returns counts
-counts = result.get_counts(circuit)
-print("\nTotal count for 00 and 11 are:",counts)
+    return out_counts
 
-# Draw the circuit
-circuit.draw()
