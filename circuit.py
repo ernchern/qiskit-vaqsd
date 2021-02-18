2ec3e2483ef876feb427b5690443a0e2e2c7120d
MINSOO KANG <stengine2@gmail.com>
2021-02-18 19:14:48 +0900

    Unfinished

--- circuit.py
+++ circuit.py
@@ -6,28 +6,32 @@ from qiskit import(
   execute,
   Aer)
 
-
-def build_circuit(params,shots=1000):
+def build_circuit(params, a_1, b_1, a_2, b_2, shots = 1000, verbose = False):
     '''
     Inputs:
         measure_index: index of a qubit to measure
         params: numpy array of [theta, phi, lam]
         shots: number of executions of the circuit
+		a_1:
+		b_1:
+		a_2:
+		b_2:
     Output:
         out_counts: numpy array of probabilities of outcomes in the order: '00', '01', '10', '11'
     '''
     # Use Aer's qasm_simulator
     simulator = Aer.get_backend('qasm_simulator')
-
     # Create a Quantum Circuit acting on the q register
     circuit1 = QuantumCircuit(1, 1)
 
+    circuit1.u3(2*np.arccos(a_1),np.arccos(np.real(b_1)/abs(b_1)),0,0)
+	
     circuit1.u3(params[0],params[1],params[2],0)
     
     circuit1.measure([0], [0])
     
     # Execute the circuit on the qasm simulator
-    job1 = execute(circuit1, simulator, shots=1000)
+    job1 = execute(circuit1, simulator, shots = shots)
 
     # Grab results from the job
     result1 = job1.result()
@@ -39,14 +43,16 @@ def build_circuit(params,shots=1000):
     # Create a Quantum Circuit acting on the q register
     circuit2 = QuantumCircuit(1, 1)
 
-    circuit2.h(0)
+    #circuit2.h(0)
+	
+    circuit2.u3(2*np.arccos(a_2),0,0,0)
     
     circuit2.u3(params[0],params[1],params[2],0)
     
     circuit2.measure([0], [0])
     
     # Execute the circuit on the qasm simulator
-    job2 = execute(circuit2, simulator, shots=1000)
+    job2 = execute(circuit2, simulator, shots = shots)
 
     # Grab results from the job
     result2 = job2.result()
@@ -54,6 +60,9 @@ def build_circuit(params,shots=1000):
     # Returns counts
     counts2 = result2.get_counts(circuit2)
     
+    if verbose:
+        print(circuit1)
+        print(circuit2)
 
     for i in ['0', '1']:
         if not i in counts1:
@@ -67,4 +76,3 @@ def build_circuit(params,shots=1000):
     out_prob = (counts1['0']+counts2['1'])/(2*shots)
 
     return out_prob
-
--- optimizer.py
+++ optimizer.py
@@ -4,44 +4,47 @@ import numpy as np
 
 # counts_distribution =  circuit.evaluate(parameter, shot_size)
 
-evaluate = circuit.build_circuit
+#evaluate = lambda params, shot_size: (circuit.build_circuit(params,0,shot_size/2)[1]+circuit.build_circuit(params,1,shot_size/2)[0])/2
 
-def optimize_circuit(iteration_limit, initial_step_size, shot_size, minimum_step):
+evaluate = lambda param, shot_size :circuit.build_circuit(param, np.sqrt(1/3),np.sqrt(2/3),shot_size) 
+
+def optimize_circuit(iteration_limit, initial_step_size, shot_size, minimum_step,verbose = True):
 	
 	iteration = 0
 	# initial input
 	parameters = np.random.rand(3) * 2 * np.pi
+	
 	# evaluate with initial input
 	reference_result = evaluate(parameters, shot_size)
-	
 	step_size = initial_step_size
 	
 	search = True
-	
 	while(search):
-		# take steps
+		# Take steps
 		variations = create_variations(parameters, step_size)
-		# evaluate and compare
+		
+		# Evaluate all the variations
 		result = []
 		for step in variations:
 			result.append(evaluate(step, shot_size))
 		
-		# change current state to the optimal step and retry
-		# if it's local optima, try smaller step
-		# check termination criteria
+		# Change current parameters to the best among 6
 		result = np.array(result)
-		print(result, end = "\t")
 		iteration += 1
+		
+		# If it's local optima, try smaller step
 		if result.max() > reference_result:
 			step_size = (reference_result - result.max())/step_size
 			reference_result = result.max()
 			parameters = variations[result.argmax()]
-			print(step_size)
-		#elif step_size > minimum_step:
-			#step_size = step_size / 2
+			if verbose:
+				print(result, "\t", step_size, "\t", reference_result)
+				print(evaluate(parameters,1000))
+		# check termination criteria
 		elif iteration_limit < iteration or step_size < minimum_step:
+			#termination condition
 			search = False
-	print()
+		print(iteration, "/1000", end = "\r")
 	return parameters, reference_result
 		
 		
@@ -55,4 +58,6 @@ def create_variations(parameters, step_size):
 		nar([parameters[0], parameters[1], parameters[2] - step_size])]
 		
 if __name__ == "__main__":
-	print(optimize_circuit(1000, 0.1, 7000, 0.0000001) )
+	for i in range(30):
+		print(optimize_circuit(1000, 0.001, 7000, 0.0000001, False))
+	
